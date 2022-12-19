19b8192ba329bb94d1a14f3b5aebb77f4e3205f8
brunomdr <brunomdr46@gmail.com>
2022-12-19 17:34:18 -0300

    Invoking Currency Exchange from Currency Conversion Microservice

--- currency-conversion-service/src/main/java/com/bruno/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/bruno/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,20 +1,42 @@
 package com.bruno.microservices.currencyconversionservice.controllers;
 
 import com.bruno.microservices.currencyconversionservice.models.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    private final String EXCHANGE_SERVICE_URL = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        HashMap<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity =
+                new RestTemplate().getForEntity(EXCHANGE_SERVICE_URL, CurrencyConversion.class, uriVariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()
+        );
     }
 }
